54a1d8cbb5a6420b367f822c89d4f14f9cb6a99c
Nayembot12 <xmking50@gmail.com>
2024-09-28 23:58:23 +0600

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -89,8 +89,8 @@ let path = __dirname + `/cache/help.png`;
   fs.writeFileSync(path, Buffer.from(image, "utf-8"));*/
 
 
-    const text = `╰➤ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ${arrayInfo.length}\n𝗨𝗦𝗘【 ${prefix}Help 】𝐏𝐀𝐆𝐄 𝐍𝐎.\n╰➤ «𝐍𝐔𝐌𝐁𝐄𝐑 𝐎𝐅 𝐏𝐀𝐆𝐄» ${page}
-╰➤ https://www.facebook.com/Imon.132233`;
+    const text = `╰➤ 𝗧𝗢𝗧𝗔𝗟 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 ${arrayInfo.length}\n𝗨𝗦𝗘【 ${prefix}Help 】𝐏𝐀𝐆𝐄 𝐍𝐎.\n╰➤ «𝐍𝐔𝐌𝐁𝐄𝐑 𝐎𝐅 𝐁𝐎𝐒𝐒» ${page}
+╰➤ https://www.facebook.com/NAYEM.KING.OKY`;
     return api.sendMessage(`╭──«~»──CMD──«~»──╮\n\nPAGE ${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}` + "\n" + msg + "\n" + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 100000));

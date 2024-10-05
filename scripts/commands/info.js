b7bda9b65ea00f9af740138d6b2a08b420ef5a1d
mdsakibahmed2006 <mdsakibahmed20062006@gmail.com>
2024-10-05 19:10:33 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -4,12 +4,12 @@ module.exports.config = {
     permission: 0,
     credits: "nayan",
     prefix: true,
-    description: "search results on google",
-    category: "without prefix",
-    usages: "google [text]",
+    description: "",
+    category: "prefix",
+    usages: "",
     cooldowns: 5,
     dependencies: 
-	{
+    {
     "request":"",
     "fs-extra":"",
     "axios":""
@@ -20,31 +20,41 @@ const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
 const time = process.uptime(),
-		hours = Math.floor(time / (60 * 60)),
-		minutes = Math.floor((time % (60 * 60)) / 60),
-		seconds = Math.floor(time % 60);
+        hours = Math.floor(time / (60 * 60)),
+        minutes = Math.floor((time % (60 * 60)) / 60),
+        seconds = Math.floor(time % 60);
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
-  
-var callback = () => api.sendMessage({body:`
---------------------------------------------
---------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐌𝐝 𝐍𝐚𝐲𝐞𝐦 𝐊𝐡𝐚𝐧𝐝𝐨𝐤𝐞𝐫😈
-😾🫦
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-🫰😘
- 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐏𝐓𝐊.
- 😈❤️
-𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-😔🥵
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-🥺💋
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-😐🤦‍♂️
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐈𝐧𝐛𝐨𝐱
-😝🙈
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/NAYEM.KING.OK`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+
+var callback = () => api.sendMessage({body:`     ⊰᯽⊱┈──╌❊╌──┈⊰᯽⊱
+۞ 𝗕𝗢𝗧 𝗔𝗗𝗠𝗜𝗡 𝗜𝗡𝗙𝗢 ۞
+◎ ══════ ❈ ══════ ◎
+╭─╮ 
+  🔥 
+╰─╯𝗡𝗔𝗠𝗘 👉𝗦𝗔𝗞𝗜𝗕 𝗠𝗔𝗛𝗠𝗨𝗗
+╭─╮ 
+  🔥 
+╰─╯𝗔𝗗𝗗𝗥𝗘𝗦𝗦👉𝗞𝗢𝗟𝗞𝗔𝗧𝗔
+╭─╮ 
+  🔥 
+╰─╯𝗪𝗛𝗔𝗧𝗦,𝗔𝗣𝗣👉01310762595
+╭─╮ 
+  🔥 
+╰─╯𝗥𝗘𝗟𝗔𝗧𝗜𝗢𝗡👉𝗦𝗜𝗡𝗚𝗘𝗟
+
+   BOT OWNER FB ID
+[ANY HELP CONTACT FB]
+
+ ➷➷➷➷➷➷➷➷➷➷➷➷➷➷ https://www.facebook.com/SAKIB.MAHMUD.V1
+
+⊰᯽⊱┈──╌❊╌──┈⊰᯽⊱
+┏•━•━•━ ◎ ━•━•━•┓
+ ❂ 𝗕𝗢𝗧  𝗢𝗪𝗡𝗘𝗥 ❂
+┗•━•━•━ ◎ ━•━•━•┛
+╭────────────╮
+✜ 𝗦𝗔𝗞𝗜𝗕 𝗠𝗔𝗛𝗠𝗨𝗗 ✜
+╰────────────╯`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://i.postimg.cc/ZnbXhYTY/inbound2931345345072188232.jpg`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
